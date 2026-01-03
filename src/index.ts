3334a42252988cc912a06cf42bbbdc7c181857e5
komeilm76 <komeilmohammadian1376.web@gmail.com>
2026-01-04 00:38:22 +0330

    feat(add support both v3,v4 zod)

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,3 @@
-import z from 'zod';
 import lib from './lib';
 const kmApi = lib as typeof lib;
 export default kmApi;
-
-const schema = kmApi.makeApiConfig({
-  path: `/origin`,
-  method: 'get',
-  tags: ['#auth', '#test'],
-  auth: 'YES',
-  disable: 'NO',
-  request: {
-    body: z.object({
-      name: z.string(),
-      age: z.number(),
-    }),
-    params: z.object({
-      page: z.number(),
-    }),
-    query: z.object({
-      search: z.string(),
-    }),
-  },
-  response: {
-    success: z.array(
-      z.object({
-        date: z.string(),
-        temperatureC: z.number().int(),
-        temperatureF: z.number().int(),
-        summary: z.string().nullable(),
-      })
-    ),
-    error: z.object({}),
-  },
-});
