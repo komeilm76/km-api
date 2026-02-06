ef8f1af4592cc7c8ff8b4a46c145cee3c9a21429
komeilm76 <komeilmohammadian1376.web@gmail.com>
2026-02-06 16:37:45 +0330

    fix(remove unused files)

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,44 @@ import lib from './lib';
 const kmApi = lib as typeof lib;
 export default kmApi;
 
-const ccc = kmApi.v43.makeApiConfig({
-  method: 'get',
-  path: '/test',
-  request: {
-    body: z.object({
-      test_body: z.string(),
-    }),
-    params: z.object({
-      test_params: z.string(),
-    }),
-    query: z.object({
-      test_query: z.string(),
-    }),
-    headers: z.object({
-      test_headers: z.string(),
-    }),
-    cookies: z.object({
-      test_cookies: z.string(),
-    }),
-  },
-  response: {
-    success: z.object({}),
-    error: z.object({}),
-  },
-  auth: 'YES',
-  disable: 'NO',
-  summary: '',
-  description: '',
-  tags: ['#s'],
-  requestContentType: 'multipart/form-data',
-  responseContentType: 'multipart/form-data',
-});
+// const ccc = kmApi.v43.makeApiConfig({
+//   method: 'get',
+//   path: '/test',
+//   request: {
+//     body: z.object({
+//       test_body: z.string(),
+//     }),
+//     params: z.object({
+//       test_params: z.string(),
+//     }),
+//     query: z.object({
+//       test_query: z.string(),
+//     }),
+//     headers: z.object({
+//       test_headers: z.string(),
+//     }),
+//     cookies: z.object({
+//       test_cookies: z.string(),
+//     }),
+//   },
+//   response: {
+//     success: z.object({}),
+//     error: z.object({}),
+//   },
+//   auth: 'YES',
+//   disable: 'NO',
+//   summary: '',
+//   description: '',
+//   tags: ['#s'],
+//   requestContentType: 'multipart/form-data',
+//   responseContentType: 'multipart/form-data',
+// });
 
-const x = kmApi.v43.adapters.safeConvertRequestBody(
-  { name: 'mohammad' },
-  'application/octet-stream'
-);
-console.log('x', x);
+// const x = kmApi.v43.adapters.safeConvertRequestBody(
+//   { name: 'mohammad' },
+//   'application/octet-stream'
+// );
+// console.log('x', x);
 
-const y = ccc.convertResponseType('alova-axios');
-console.log('y', y);
+// const y = ccc.convertResponseType('alova-axios');
+// console.log('y', y);
--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,13 +1,9 @@
 import v3 from './v3';
 import v4 from './v4';
-import v41 from './v4.1';
-import v42 from './V4.2';
 import v43 from './v4.3';
 
 export default {
   v3: v3 as typeof v3,
   v4: v4 as typeof v4,
-  v41: v41 as typeof v41,
-  v42: v42 as typeof v42,
   v43: v43 as typeof v43,
 };
